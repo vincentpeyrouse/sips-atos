65714c5c4409e4d7eb12b279bb1e99a92f254dab
Vincent PEYROUSE <Vincent@MacBook-Pro-Retina-de-Vincent.local>
2013-10-31 10:57:44 +0100

    Test server

--- bin/server.js
+++ bin/server.js
@@ -1,33 +1,17 @@
 /*jslint node: true es5:true nomen:true*/
 
-var AtosSIPS = require('../'),
-    sips = new AtosSIPS();
+var express = require('express'),
+    http = require('http');
 
-var http = require('http');
+var app = express();
 
-http.createServer(function (req, res) {
-    if (req.url === '/return' && req.method === 'POST') {
-        var buffer = '';
-        req.on('data', function (chunk) {
-            buffer += chunk.toString();
-        });
-        req.on('end', function () {
-            sips.response(buffer.replace('DATA=', ''), function (err, data) {
-                console.log(err, data);
-            });
-        });
-    } else if (req.url === '/') {
-        sips.request({
-            merchant_id: '013044876511112',
-            amount: '100',
-            customer_id: 'yolo',
-            cancel_return_url: 'http://localhost:1337/return',
-            normal_return_url: 'http://localhost:1337/return',
-            automatic_response_url: 'http://localhost:1337/return'
-        }, function (err, data) {
-            console.log(err, data);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data);
-        });
-    }
-}).listen(1337);
+/**
+ * Server setup.
+ */
+require('../lib/server')(app);
+
+http.createServer(app).listen(app.get('port'), function () {
+    'use strict';
+    
+    console.log('AtosSIPS server started on port ' + app.get('port'));
+});
